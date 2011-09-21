31ec15a6f58390dd85fc5066972c5e621efba1aa
Joe Febrian <ri_hanz@yahoo.co.id>
2011-09-21 12:22:03 +0700

    file website EDuS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Percobaan::Application.config.secret_token = '7882410db5e4b5661ecb89d2016475b2e6ef09596d398d625060542a18fb8de2f6609b27305785b7e041811f33ad3e11685369ff642ffdb5462e5f1c296a20b9'
